2dbf5528d6edb7303938d8d641daea129f3dbf8b
OlegSm31kov <olegsm31kov@yandex.ru>
2026-06-22 00:34:28 +0300

    cleared dialogue_manager.py structure

--- services/dialogue_manager.py
+++ services/dialogue_manager.py
@@ -1,6 +1,4 @@
 import random
-from itertools import product
-from sys import intern
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
@@ -17,23 +15,8 @@ class DialogManager:
         self.retriever = retriever
 
     async def handle_message(self, replica, update, context):
-        print(f"replica: {replica}")
+        print(f"\nreplica: {replica}")
 
-        intent, confidence = self.classifier.predict(replica)
-        print(f"intent: {intent}\nconfidence: {confidence}")
-        if confidence > 0.3:
-            await self._process_intent(intent, replica, update, context)
-            return
-
-        # пытаемся обработать текущий шаг в рекомендации и покупке диска
-        step = context.user_data.get("step")
-        print(f"step: {step}")
-        if step:
-            handled = await self._process_step(replica, update, context)
-            if handled:
-                return
-
-        # если к покупке ещё не перешли, то обрабатываем общее состояние диалога
         dialog_state = context.user_data.get("dialog_state")
         print(f"dialog_state: {dialog_state}")
         if dialog_state:
@@ -41,12 +24,17 @@ class DialogManager:
             if handled:
                 return
 
+        intent, confidence = self.classifier.predict(replica)
+        print(f"intent: {intent}\nconfidence: {confidence}")
+        if confidence > 0.3:
+            await self._process_intent(intent, replica, update, context)
+            return
+
         await self._process_intent("smalltalk", replica, update, context)
         return
 
     async def _process_dialog_state(self, replica, update, context):
         state = context.user_data.get("dialog_state")
-        print(f"dialog_state: {state}")
         match state:
             case "greeting":
                 context.user_data["dialog_state"] = "asking_hobby"
@@ -78,115 +66,27 @@ class DialogManager:
                 await update.message.reply_text(self._build_advertising_message(hobby, top_product), parse_mode="HTML")
                 await show_showcase_menu(update, top_product, context)
 
-                context.user_data["step"] = None
+                context.user_data["dialog_state"] = None
                 return True
-
-            case "awaiting_ad_response":
-                intent = (self.classifier.predict(replica))[0]
-                print(f"intent: {intent}")
-
-                if intent == "yes":
-                    if self._can_recommend(context.user_data):
-                        products = recommend_products(context.user_data)[:3]
-                        await self._give_recommendations(update, products)
-                        return True
-
-                    context.user_data["dialog_state"] = None
-
-                    if "use_case" not in context.user_data:
-                        await update.message.reply_text("Отлично, для каких задач нужен диск?")
-                        context.user_data["step"] = "awaiting_use_case"
-                        return True
-
-                    if "budget" not in context.user_data:
-                        context.user_data["step"] = "awaiting_budget"
-                        await update.message.reply_text("Каков ваш бюджет?")
-                        return True
-
-                if intent == "no":
-                    context.user_data["dialog_state"] = None
-                    await update.message.reply_text(
-                        "Хорошо\nЕсли у вас закончится место и понадобится помощь "
-                        "с выбором накопителя — я всегда к вашим услугам"
-                    )
-                    return True
-
-                await self._process_intent(intent, replica, update, context)
-                return True
-
-            case "after_showcase":
-                intent = self.classifier.predict(replica)[0]
-
-                if intent == "show_more":
-                    products = recommend_products(context.user_data)[1:4]
-                    await self._give_recommendations(update, products)
-
-                elif intent == "no":
-                    await update.message.reply_text(
-                        "Хорошо\nЕсли у вас закончится место и понадобится помощь "
-                        "с выбором накопителя — я всегда к вашим услугам"
-                    )
-                    return True
-
-            case "advertising":
-                entities = extract_entities(replica)
-                context.user_data.update(entities)
-
-                if self._can_recommend(context.user_data):
-                    products = recommend_products(context.user_data)[:3]
-                    await self._give_recommendations(update, products)
-                    return True
-
-                context.user_data["step"] = "awaiting_budget"
-
-                await update.message.reply_text(
-                    "Кстати, если понадобится диск, "
-                    "какой у вас бюджет?"
-                )
-
-                return True
-        return True
-
-    async def _process_step(self, replica, update, context):
-        step = context.user_data.get("step")
-        match step:
-            case "awaiting_use_case":
-                self._update_user_data(replica, context)
-
-                if self._can_recommend(context.user_data):
-                    products = recommend_products(context.user_data)[:3]
-                    await self._give_recommendations(update, products)
-                    context.user_data["step"] = None
-                    return True
-
-                if "use_case" not in context.user_data:
-                    await update.message.reply_text("Укажите назначение диска: "
-                                                    "игры, видео, архив или система")
-                    return True
-
-                if "budget" not in context.user_data:
-                    await update.message.reply_text("Каков ваш бюджет?")
-                    context.user_data["step"] = "awaiting_budget"
-                    return True
 
             case "awaiting_budget":
-                self._update_user_data(replica, context)
+                entities = extract_entities(replica)
+                context.user_data.update(entities)
+                print(f"entities: {context.user_data}")
+                if "budget" in context.user_data:
+                    products = recommend_products(context.user_data)[:3]
+                    await self._give_recommendations(update, products)
+                    context.user_data["dialog_state"] = None
+                    return
 
-                if "budget" not in context.user_data:
-                    await update.message.reply_text("Укажите бюджет числом")
+                else:
+                    await update.message.reply_text("Каков ваш бюджет?")
                     return True
 
-                products = recommend_products(context.user_data)[:3]
-                await self._give_recommendations(update, products)
-                context.user_data["step"] = None
-                return True
+        return True
 
-
-
-        return False
 
     async def _process_intent(self, intent, replica, update, context):
-        print(f"intent: {intent}")
         match intent:
             case "greet":
                 context.user_data["dialog_state"] = "greeting"
@@ -196,18 +96,19 @@ class DialogManager:
             case "recommend":
                 entities = extract_entities(replica)
                 context.user_data.update(entities)
+                print(f"entities: {context.user_data}")
                 if self._can_recommend(context.user_data):
                     products = recommend_products(context.user_data)[:3]
                     await self._give_recommendations(update, products)
                     return
 
                 if "budget" not in context.user_data:
-                    context.user_data["step"] = "awaiting_budget"
                     await update.message.reply_text("Каков ваш бюджет?")
+                    context.user_data["dialog_state"] = "awaiting_budget"
                     return
 
                 if "use_case" not in context.user_data:
-                    context.user_data["step"] = "awaiting_use_case"
+                    context.user_data["dialog_state"] = "awaiting_use_case"
                     await update.message.reply_text("Для каких задач нужен диск?")
                     return
 
@@ -218,8 +119,8 @@ class DialogManager:
                     await update.message.reply_text(answer)
                     if not hobby_asked:
                         if random.choice([True, False, True]):
-                            context.user_data["dialog_state"] = "asking_hobby"
                             await update.message.reply_text(random.choice(HOBBY_QUESTIONS))
+                            context.user_data["dialog_state"] = "asking_hobby"
                             context.user_data["hobby_asked"] = True
                             return
                 else:
@@ -278,7 +179,7 @@ class DialogManager:
         ]
 
         count = sum(key in user_data for key in useful_entities)
-        return count >= 2
+        return count >= 1
 
 async def show_showcase_menu(update, product, context):
     keyboard = [
@@ -301,11 +202,6 @@ async def showcase_callback(update, context):
 
     match query.data:
 
-        case "buy_current":
-            await query.message.reply_text(
-                "Отлично! Для покупки перейдите по ссылке ..."
-            )
-
         case "show_more":
             products = recommend_products(context.user_data)[1:4]
             answer = "Другие варианты:\n\n"
@@ -317,6 +213,4 @@ async def showcase_callback(update, context):
             await query.message.reply_text(answer, parse_mode="HTML")
 
         case "not_interested":
-            await query.message.reply_text(
-                "Хорошо\n Если понадобится помощь с выбором накопителя, обращайтесь!"
-            )
+            await query.message.reply_text("Хорошо\nЕсли понадобится помощь с выбором накопителя, обращайтесь!")
